81f38f64d6311b8f44ceb9b36cc3c2f42fb801af
lalitrn44 <lalitrn44@gmail.com>
2024-01-20 01:30:58 +0530

    feat!: replaced eslint+prettier with biomejs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/tailwind-utils";
 import "@/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
 
 export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
+	subsets: ["latin"],
+	variable: "--font-sans",
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<head />
+			<body
+				className={cn(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable,
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
