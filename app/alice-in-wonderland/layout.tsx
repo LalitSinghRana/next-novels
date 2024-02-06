d9ea8b729306c2d83bc32fdec6204f0913033ef6
lalitrn44 <lalitrn44@gmail.com>
2024-02-06 22:25:49 +0530

    feat(alice): title page text

--- app/alice-in-wonderland/layout.tsx
+++ app/alice-in-wonderland/layout.tsx
@@ -1,21 +1,51 @@
-import { cn } from "@/lib/tailwind-utils";
-import "@/styles/globals.css";
-import { Inter as FontSans } from "next/font/google";
+"use client";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
-const fontSans = FontSans({
-	subsets: ["latin"],
-	variable: "--font-sans",
+const AlertDialogContent = dynamic(() => import("@/components/ui/alert-dialog").then((mod) => mod.AlertDialogContent), {
+	ssr: false,
 });
+
+function useIsMobileDevice() {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		setIsMobile(
+			window.screen.orientation.type === "portrait-primary" || window.screen.orientation.type === "portrait-secondary",
+		);
+	}, []);
+
+	return isMobile;
+}
 
 export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	if (useIsMobileDevice()) {
+		return children;
+	}
+
 	return (
-		<html lang="en" suppressHydrationWarning>
-			<head />
-			<body className={cn("h-screen bg-background w-screen font-sans antialiased", fontSans.variable)}>{children}</body>
-		</html>
+		<AlertDialog defaultOpen>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Currently desktop browsers are not supported</AlertDialogTitle>
+					<AlertDialogDescription>Visit this page on a mobile device to read this story.</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogAction>Go back to Home</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
 	);
 }
