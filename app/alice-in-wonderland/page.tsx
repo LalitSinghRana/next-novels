18a79f7e6d6709a880b1f375fdd5cd7315d51197
Lalit Singh Rana <lalitrn44@gmail.com>
2024-02-07 21:11:03 +0530

    feat(alice): title page text (#10)

--- app/alice-in-wonderland/page.tsx
+++ app/alice-in-wonderland/page.tsx
@@ -4,6 +4,10 @@ const BackgroundLayer = () => {
 	return <div className="background" />;
 };
 
+const BackdropLayer = () => {
+	return <div className="backdrop" />;
+};
+
 const TextLayer = () => {
 	return <p className="text">Alice's Adventures in Wonderland</p>;
 };
@@ -12,6 +16,7 @@ export default function Home() {
 	return (
 		<div className="flex w-full h-full justify-center items-center">
 			<TextLayer />
+			{/* <BackdropLayer /> */}
 			<BackgroundLayer />
 		</div>
 	);
