53111c8838612c9122269b2ea15b4307c4f67034
lalitrn44 <lalitrn44@gmail.com>
2024-02-28 23:18:33 +0530

    closing PR

--- app/alice-in-wonderland/chapter/1/page.tsx
+++ app/alice-in-wonderland/chapter/1/page.tsx
@@ -1,6 +1,6 @@
 const Scene1 = () => {
 	return (
-		<div className="flex w-screen h-screen justify-center items-center p-4">
+		<div className="w-screen h-screen flex justify-center items-center p-4 bg-lime-200">
 			<p>
 				Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or
 				twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, “and
@@ -12,19 +12,23 @@ const Scene1 = () => {
 
 const Scene2 = () => {
 	return (
-		<p>
-			So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and
-			stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the
-			daisies, when suddenly a White Rabbit with pink eyes ran close by her.
-		</p>
+		<div className="w-screen h-screen flex justify-center items-center p-4 bg-indigo-400">
+			<p>
+				So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and
+				stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the
+				daisies, when suddenly a White Rabbit with pink eyes ran close by her.
+			</p>
+		</div>
 	);
 };
 
 export default function Chapter() {
 	return (
-		<div className="flex w-screen h-screen justify-center items-center p-4">
-			<Scene1 />;
-			<Scene2 />;
+		<div>
+			<Scene1 />
+			<Scene2 />
+			<Scene1 />
+			<Scene2 />
 		</div>
 	);
 }
